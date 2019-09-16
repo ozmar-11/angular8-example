8e146e3670ae5ae99d429bf6a635e8493b3f4f9a
Ozmar Ugarte <ozmar_11@hotmail.com>
2019-09-15 20:30:05 -0500

    Angular 8 getting started exercise

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name:  'Phone XL',
+    price: 799,
+    description: 'A large phone width one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone width one of the best cameras'
+  },
+  {
+    name: 'Phone standard',
+    price: 299,
+    description: ''
+  }
+];
